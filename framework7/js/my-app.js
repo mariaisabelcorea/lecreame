0f3ab30c1b51362879b121f6373e0794ce728ba8
mariaisabelcorea <42661899+mariaisabelcorea@users.noreply.github.com>
2018-12-12 18:30:48 -0600

    Add files via upload

---
+++ framework7/js/my-app.js
@@ -0,0 +1,51 @@
+// Initialize your app
+var myApp = new Framework7({
+	root:'#app',
+	theme: 'md', //ios, md, auto
+    modalTitle: 'Framework7', 	//title for modals
+	panel: {
+    	swipe: 'left',
+  	},
+	cache:false,
+	pushState:false,
+	swipeBackPage:true,
+	material: true,
+	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		},
+		{
+		path: '/cupones/',
+    	url: 'cupones.html',
+    	name: 'cupones',
+  		},
+		{
+		path: '/info-cupon/',
+    	url: 'info-cupon.html',
+    	name: 'info-cupon',
+  		},
+		{
+		path: '/comidas/',
+    	url: 'comidas.html',
+    	name: 'comidas',
+  		},
+		{
+		path: '/info-comida/',
+    	url: 'info-comida.html',
+    	name: 'info-comida',
+  		}
+		
+	]
+});
+
+// Export selectors engine
+var $$ = Dom7;
+
+
+
+
+
+ 
+
